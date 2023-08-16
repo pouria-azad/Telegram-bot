6801b964c180467878db13c95aaa501360a6da38
pouria <109802957+pouria-azad@users.noreply.github.com>
2023-08-16 20:15:39 +0330

    Update get.php

--- get.php
+++ get.php
@@ -14,5 +14,16 @@ if($Message_entities != false){
         startWellcome($Message_id , "با سلام به ربات یادآور خوش آمدید.\nلطفا یکی از گزینه های زیر را انتخاب نمایید:" , $Keyboard , $Message_message_id);
 
 }}
+$switch = false;
+if($Object['message']['text'] == 'عضویت در گروه یادآور' && $switch == false){
+    $switch = true;
+    sendMessage($Message_id , 'لطفا نام و نام خانوادگی خود را ارسال نمایید:');
+}
+if ($switch == true){
+    $text = 'نام و نام خانودادگی شما: '.$Object['message']['text'].' ثبت شد.';
+    sendMessage($Message_id, $text);
+    $switch = false
+
+}
 
 //sendMessage($Message_id, $Content);
