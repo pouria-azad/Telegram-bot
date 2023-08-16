173988ca0684f2e340e121dcfb20e81fbefad665
pouria <109802957+pouria-azad@users.noreply.github.com>
2023-08-16 19:54:40 +0330

    reply user

--- function.php
+++ function.php
@@ -8,10 +8,10 @@ function sendMessage($Message_id , $text){
     file_get_contents($Request_to_server);
 }
 
-function startWellcome($Message_id , $text , $keyboard){
+function startWellcome($Message_id , $text , $keyboard , $Message_message_id){
     $Method = 'sendMessage';
     $arr_keyboard = array("keyboard"=>$keyboard , "resize_keyboard"=>true , "one_time_keyboard"=>true);
     $reply_markup = json_encode($arr_keyboard);
-    $Request_to_server = API_REQUEST.$Method."?"."chat_id=".$Message_id."&"."text=".$text."&"."reply_markup=".$reply_markup;
+    $Request_to_server = API_REQUEST.$Method."?"."chat_id=".$Message_id."&"."text=".$text."&"."reply_markup=".$reply_markup."&"."reply_to_message_id=".$Message_message_id;
     file_get_contents($Request_to_server);
 }
--- get.php
+++ get.php
@@ -3,12 +3,17 @@ include './function.php';
 $Content = file_get_contents('php://input');
 $Object = json_decode($Content , true);
 $Message_id = $Object['message']['from']['id'];
+$Message_message_id = $Object['message']['message_id'];
+
 
 $Message_entities = $Object['message']['entities'] ?? false;
+
 if($Message_entities != false){
     if($Object['message']['text'] == '/start'){
-        $keyboard = [ ['عضویت در گروه یادآور'] , ['درباره'] ];
-        startWellcome($Message_id , "test" , $keyboard);
+        $Keyboard = [ ['عضویت در گروه یادآور'] , ['درباره'] ];
+        startWellcome($Message_id , "با سلام به ربات یادآور خوش آمدید.
+        لطفا یکی از گزینه های زیر را انتخاب نمایید:" , $Keyboard , $Message_message_id);
+
 }}
 
 //sendMessage($Message_id, $Content);
