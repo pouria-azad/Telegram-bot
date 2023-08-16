767933033beecce93e984de094b5acb37724e6ac
pouria <109802957+pouria-azad@users.noreply.github.com>
2023-08-16 19:33:52 +0330

    update

--- function.php
+++ function.php
@@ -2,8 +2,15 @@
 define('API_TOKEN', '6546866682:AAGJD4uISP2U4RsOU8JC0fWNZ8VirfjGZnE');
 define('API_REQUEST', 'https://api.telegram.org/bot'.API_TOKEN.'/');
 
-function sendMessage($Chat_id , $Message){
+function sendMessage($Message_id , $text){
     $Method = 'sendMessage';
-    $Request_to_server = API_REQUEST.$Method."?"."chat_id=".$Chat_id."&"."text=".$Message;
+    $Request_to_server = API_REQUEST.$Method."?"."chat_id=".$Message_id."&"."text=".$text;
+    file_get_contents($Request_to_server);
+}
+
+function startWellcome($Message_id , $text , $keyboard){
+    $Method = 'sendMessage';
+    $reply_markup = json_encode($keyboard);
+    $Request_to_server = API_REQUEST.$Method."?"."chat_id=".$Message_id."&"."text=".$text."&"."reply_markup=".$reply_markup;
     file_get_contents($Request_to_server);
 }
--- get.php
+++ get.php
@@ -2,6 +2,13 @@
 include './function.php';
 $Content = file_get_contents('php://input');
 $Object = json_decode($Content , true);
-$Chat_id = $Object['message']['chat']['id'];
+$Message_id = $Object['message']['from']['id'];
 
-sendMessage($Chat_id, $Content);
+$Message_entities = $Object['message']['entities'] ?? false;
+if($Message_entities != false){
+    if($Object['message']['text'] == '/start'){
+        $keyboard = [ ['عضویت در گروه یادآور'] , ['درباره'] ];
+        startWellcome($Message_id , $text , $keyboard);
+}}
+
+//sendMessage($Message_id, $Content);
