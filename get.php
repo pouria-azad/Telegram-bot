0cb626da459fa3c887a1ab15c37ab11f53c4ccc6
pouria <109802957+pouria-azad@users.noreply.github.com>
2023-08-16 17:59:48 +0330

    Update get.php

--- get.php
+++ get.php
@@ -2,6 +2,6 @@
 include './function.php';
 $Content = file_get_contents('php://input');
 $Object = json_decode($Content , true);
-$Chat_id = $Object['message']['chat']['id'];
+$Chat_id = $Object->message->chat->id;
 
 sendMessage($Chat_id, 'hi');
