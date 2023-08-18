0dffea214e3efdf71f28b695c68556a89cb281b8
pouria <109802957+pouria-azad@users.noreply.github.com>
2023-08-18 15:11:05 +0330

    Update tst.php

--- tst.php
+++ tst.php
@@ -1,7 +1,17 @@
 <?php
 include "./config.php";
-$sql = "INSERT INTO `users` (`chat_id`, `username`, `entry_year`, `fullname`, `status`) VALUES (?, NULL, NULL, NULL, NULL);";
-$stml = $conn->prepare($sql);
-$chat_id = 123456789;
-$stmt->bind_param($chat_id);
-echo $stmt->execute();
+// $sql = "INSERT INTO `users` (`chat_id`, `username`, `entry_year`, `fullname`, `status`) VALUES (?, NULL, NULL, NULL, NULL);";
+// $stml = $conn->prepare($sql);
+// $chat_id = 123456789;
+// $stmt->bind_param($chat_id);
+// echo $stmt->execute();
+
+$sql = "SELECT `chat_id` FROM  `users`";
+$stmt = $conn->prepare($sql); 
+$stmt->execute();
+$result = $stmt->setFetchMode(PDO::FETCH_ASSOC); 
+foreach($stmt->fetchAll() as $k=>$v) 
+{
+    echo $v;
+  }
+
