0c9d577ac6b673721284bd7a48798dee8ac6373d
pouria <109802957+pouria-azad@users.noreply.github.com>
2023-08-18 11:37:18 +0330

    connect php to sql

--- config.php
+++ config.php
@@ -1,10 +1,11 @@
  <?php
 $servername = "localhost";
-$username = "telegr12_users";
+$username = "telegr12_myproject";
 $password = "R9@&E7AC-m.zsG8";
+$dbname = "telegr12_myproject";
 
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   echo "Connected successfully";
