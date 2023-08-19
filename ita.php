342b7cf13d6f1f6ac3dee91ccaeb23084a5b9d9c
pouria <109802957+pouria-azad@users.noreply.github.com>
2023-08-19 18:04:31 +0330

    Update ita.php

--- ita.php
+++ ita.php
@@ -6,10 +6,10 @@ include "./config.php";
 $Message_id = "111";
 
 try {
-    $sql = "INSERT INTO `status`(`chat_id`, `status`) VALUES (:chat_id , :status )";
+    $sql = "INSERT INTO `kj` (`id`) VALUES (:chat_id);";
     $stml = $conn->prepare($sql);
     $stmt->bindValue(":chat_id", $Message_id);
-    $stmt->bindValue(":status", "0");
+    //$stmt->bindValue(":status", "0");
     // use exec() because no results are returned
     $conn->exec($sql);
     echo "New record created successfully";
