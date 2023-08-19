8b4132ae0f81154b9b8762c31d3173fc0b8dc7a7
pouria <109802957+pouria-azad@users.noreply.github.com>
2023-08-19 17:04:30 +0330

    Update get.php

--- get.php
+++ get.php
@@ -5,7 +5,7 @@ include "./config.php";
 $Content = file_get_contents('php://input');
 $Object = json_decode($Content, true);
 $Message_id = $Object['message']['from']['id'];
-$Message_message_id = $Object['message']['message_id'];
+//$Message_message_id = $Object['message']['message_id'];
 $Message_entities = $Object['message']['entities'] ?? false;
 // $Date = $Object['date'];
 
