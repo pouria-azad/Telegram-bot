02dd670f7c75f9b4c0493d66c680f10e7c78b346
pouria <109802957+pouria-azad@users.noreply.github.com>
2023-08-19 16:25:27 +0330

    Update ita.php

--- ita.php
+++ ita.php
@@ -4,12 +4,13 @@ include "./config.php";
 
 $Message_id = 1178581717;
 
-sendMessage("1178581717", "666");
+sendMessage("1178581717", "1");
 
 
     $sql = "INSERT INTO `status`(`chat_id`, `status`) VALUES ('1178581717' , '0')";
-    sendMessage("1178581717", "1");
+    sendMessage("1178581717", "2");
     $stml = $conn->prepare($sql);
     $stmt->execute();
-    $Keyboard = [ ['dfgdدرباره']];
-    startWellcome($Message_id, "با سلام به ربات یادآور خوش آمدید.\nلطفا یکی از گزینه های زیر را انتخاب نمایید:", $Keyboard, $Message_message_id);
+    sendMessage("1178581717", "3");
+    //$Keyboard = [ ['dfgdدرباره']];
+    //startWellcome($Message_id, "با سلام به ربات یادآور خوش آمدید.\nلطفا یکی از گزینه های زیر را انتخاب نمایید:", $Keyboard, $Message_message_id);
