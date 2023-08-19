6a6c2fa4e82bcc4a34d1f050978f97b7dd0518f9
pouria <109802957+pouria-azad@users.noreply.github.com>
2023-08-19 19:33:35 +0330

    Update get.php

--- get.php
+++ get.php
@@ -7,59 +7,59 @@ $Object = json_decode($Content, true);
 $Message_id = $Object['message']['from']['id'];
 $Message_message_id = $Object['message']['message_id'];
 $Message_entities = $Object['message']['entities'] ?? false;
- $Date = $Object['message']['date'];
+$Date = $Object['message']['date'];
 
 sendMessage("1178581717", "1");
 
 
-if ($Message_entities) {
-    if ($Object['message']['text'] == '/start') {
+if ($Message_entities && $Object['message']['text'] == '/start') {
 
-        sendMessage("1178581717", "2");
+
+    sendMessage("1178581717", "2");
+
+    try {
+        $sql = "SELECT `chat_id`,`status` FROM `status` WHERE `chat_id`= ?";
+        $pdo = $conn->prepare($sql);
+        $pdo->bindValue(1, $Message_id);
+        $pdo->execute();
+        $result = $pdo->setFetchMode(PDO::FETCH_ASSOC);
+        $array = $pdo->fetchAll();
+    } catch (PDOException $e) {
+        echo $sql . "<br>" . $e->getMessage();
+    }
+
+    if (!$array) {
+        try {
+            $pdo = $conn->prepare("INSERT INTO `status`(`chat_id`, `status`) VALUES (? , ?)");
+            $pdo->bindValue(1, $Message_id);
+            $pdo->bindValue(2, "0");
+            $pdo->execute();
+            sendMessage("1178581717", "New record created successfully");
+            // echo "New record created successfully";
+        } catch (PDOException $e) {
+            //   echo $sql . "<br>" . $e->getMessage();
+            sendMessage("1178581717", $sql . "<br>" . $e->getMessage());
+        }
+    } else {
 
         try {
-            $sql = "SELECT `chat_id`,`status` FROM `status` WHERE `chat_id`= ?";
-            $pdo = $conn->prepare($sql);
-            $pdo->bindValue(1, $Message_id);
-            $pdo->execute();
-            $result = $pdo->setFetchMode(PDO::FETCH_ASSOC);
-            $array = $pdo->fetchAll();
+
+            $stmt = $conn->prepare("UPDATE `status` SET `date`= ? ,`status`= ? WHERE `chat_id`= ?");
+            $stmt->bindValue(1, $Date);
+            $stmt->bindValue(2, "0");
+            $stmt->bindValue(3, $Message_id);
+            $stmt->execute();
+            // echo a message to say the UPDATE succeeded
+            echo $stmt->rowCount() . " records UPDATED successfully";
         } catch (PDOException $e) {
             echo $sql . "<br>" . $e->getMessage();
         }
-
-        if (!$array) {
-            try {
-                $pdo = $conn->prepare("INSERT INTO `status`(`chat_id`, `status`) VALUES (? , ?)");
-                $pdo->bindValue(1, $Message_id);
-                $pdo->bindValue(2, "0");
-                $pdo->execute();
-                sendMessage("1178581717", "New record created successfully");
-                // echo "New record created successfully";
-            } catch (PDOException $e) {
-                //   echo $sql . "<br>" . $e->getMessage();
-                sendMessage("1178581717", $sql . "<br>" . $e->getMessage());
-            }
-        } else {
-
-            try {
-
-                $stmt = $conn->prepare("UPDATE `status` SET `date`= ? ,`status`= ? WHERE `chat_id`= ?");
-                $stmt->bindValue(1, $Date);
-                $stmt->bindValue(2, "0");
-                $stmt->bindValue(3, $Message_id);
-                $stmt->execute();
-                // echo a message to say the UPDATE succeeded
-                echo $stmt->rowCount() . " records UPDATED successfully";
-            } catch (PDOException $e) {
-                echo $sql . "<br>" . $e->getMessage();
-            }
-        }
-        $conn = null;
-        $Keyboard = [['مدیریت لیست اعضا'], ['درباره']];
-        startWellcome($Message_id, "با سلام به ربات یادآور خوش آمدید.<br>لطفا یکی از گزینه های زیر را انتخاب نمایید:", $Keyboard, $Message_message_id);
     }
+    $conn = null;
+    $Keyboard = [['مدیریت لیست اعضا'], ['درباره']];
+    startWellcome($Message_id, "با سلام به ربات یادآور خوش آمدید.<br>لطفا یکی از گزینه های زیر را انتخاب نمایید:", $Keyboard, $Message_message_id);
 }
+
 // $switch = false;
 // if($Object['message']['text'] == 'عضویت در گروه یادآور' && $switch == false){
 //     $switch = true;
