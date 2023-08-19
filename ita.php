a8ff073f709888987a8ba5755727d53a14c9ad61
pouria <109802957+pouria-azad@users.noreply.github.com>
2023-08-19 16:29:39 +0330

    Update ita.php

--- ita.php
+++ ita.php
@@ -4,13 +4,14 @@ include "./config.php";
 
 $Message_id = 1178581717;
 
-sendMessage("1178581717", "1");
 
-
-    $sql = "INSERT INTO `status`(`chat_id`, `status`) VALUES ('1178581717' , '0')";
-    sendMessage("1178581717", "2");
-    $stml = $conn->prepare($sql);
-    $stmt->execute();
-    sendMessage("1178581717", "3");
-    //$Keyboard = [ ['dfgdدرباره']];
-    //startWellcome($Message_id, "با سلام به ربات یادآور خوش آمدید.\nلطفا یکی از گزینه های زیر را انتخاب نمایید:", $Keyboard, $Message_message_id);
+    try {
+        $sql = "INSERT INTO `status`(`chat_id`, `status`) VALUES ('1178581717' , '0')";
+        // use exec() because no results are returned
+        $conn->exec($sql);
+        echo "New record created successfully";
+      } catch(PDOException $e) {
+        echo $sql . "<br>" . $e->getMessage();
+      }
+      
+      $conn = null;
