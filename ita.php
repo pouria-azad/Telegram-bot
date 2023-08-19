3d7438056aa9c7d7c3f792697a7d7da9b870a784
pouria <109802957+pouria-azad@users.noreply.github.com>
2023-08-19 16:48:51 +0330

    Update ita.php

--- ita.php
+++ ita.php
@@ -2,16 +2,26 @@
 include './function.php';
 include "./config.php";
 
-$Message_id = 1178581717;
+
+try {
+    $sql = "INSERT INTO `kj` (`id`) VALUES (NULL)";
+    // use exec() because no results are returned
+    $conn->exec($sql);
+    echo "New record created successfully";
+  } catch(PDOException $e) {
+    echo $sql . "<br>" . $e->getMessage();
+  }
+  
+  $conn = null;
 
 
-    try {
-        $sql = "INSERT INTO `status`(`chat_id`, `status`) VALUES ('1178581717' , '0')";
-        // use exec() because no results are returned
-        $conn->exec($sql);
-        echo "New record created successfully";
-      } catch(PDOException $e) {
-        echo $sql . "<br>" . $e->getMessage();
-      }
-      
-      $conn = null;
+// try {
+//     $sql = "INSERT INTO `status`(`chat_id`, `status`) VALUES ('1178581717' , '0')";
+//     // use exec() because no results are returned
+//     $conn->exec($sql);
+//     echo "New record created successfully";
+//   } catch(PDOException $e) {
+//     echo $sql . "<br>" . $e->getMessage();
+//   }
+  
+//   $conn = null;
