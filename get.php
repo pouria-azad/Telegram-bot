a787b5a51171fb6e20c3184a2008f181a8a6a221
pouria <109802957+pouria-azad@users.noreply.github.com>
2023-08-22 02:44:29 +0330

    Update get.php

--- get.php
+++ get.php
@@ -177,7 +177,8 @@ elseif ($Callback_chat_id && $Callback_data && $is_admin[0]['status']) {
                             . "عنوان ادمین در گروه: " . $admins["custom_title"] . "%0A";
                     }
                 }
-                sendMessage($Callback_id, $base0 . $base1);
+                sendMessage($Callback_chat_id, $base0 . $base1);
+                logi($conn,"call back chat id",$Callback_chat_id , "", $Date);
                 answerCallbackQuery($Callback_id, "لیست ادمین ها با موفقیت ارسال شد!");
                 break;
         }
