04c32f6f46afed1b411f6bd1e43f5f02b82c0bea
pouria <109802957+pouria-azad@users.noreply.github.com>
2023-08-21 17:34:20 +0330

    Update function.php

--- function.php
+++ function.php
@@ -6,7 +6,7 @@ define('API_REQUEST', 'https://api.telegram.org/bot'.API_TOKEN.'/');
 function sendMessage($Message_id , $text){
     $Method = 'sendMessage';
     $Request_to_server = API_REQUEST.$Method."?"."chat_id=".$Message_id."&"."text=".$text."&"."parse_mode=HTML";
-    file_get_contents($Request_to_server);
+    return file_get_contents($Request_to_server);
 }
 
 function startWellcome($Message_id , $text , $keyboard , $Message_message_id){
