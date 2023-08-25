6281297cc735edebe0024ae97fee33eea4c8226a
pouria <109802957+pouria-azad@users.noreply.github.com>
2023-08-25 18:23:10 +0330

    Update get.php

--- get.php
+++ get.php
@@ -99,7 +99,7 @@ if (($Message_entities && $Object['message']['text'] == '/start') || (in_array($
 } //
 elseif ($array[0]['status'] == "0" && $Object['message']['text'] == 'درباره') {
     $text = "سازنده: " . "<a href='tg://user?id=1178581717'><b>" . "Pouria Azad" . "</b></a>";
-    sendMessage("1178581717", $text);
+    sendMessage($Message_id, $text);
 } elseif ($array[0]['status'] == "0" && $Object['message']['text'] == 'مدیریت لیست اعضا' && $is_admin[0]['status']) {
     changeStatus($array, $conn,  $Date, "-1", $Message_id);
     //////
