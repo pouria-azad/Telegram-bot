c5563dca03362eca187c8fb7ea2efd8b8fed922c
pouria <109802957+pouria-azad@users.noreply.github.com>
2023-08-25 19:02:37 +0330

    Update get.php

--- get.php
+++ get.php
@@ -99,7 +99,7 @@ if (($Message_entities && $Object['message']['text'] == '/start') || (in_array($
 } //
 elseif ($array[0]['status'] == "0" && $Object['message']['text'] == 'درباره') {
     sendMessage("1178581717", "Dfgdf");
-    $text = "<b>". "سازنده: " . "<a href='tg://user?id=1178581717'>" . "Pouria Azad" . "</b></a>" . " @pouria_azad";
+    $text = "<b>". "سازنده: " . "<a href='tg://user?id=1178581717'>" . "Pouria Azad" . "</a></b>" . " @pouria_azad";
     //$text2 = $text ."%0A" ."<b>"."سورس پروژه "  . "<a href='https://github.com/pouria-azad/Telegram-bot'>" . "Telegram bot" . "</b></a>";
     sendMessage($Message_id, $text);
 } elseif ($array[0]['status'] == "0" && $Object['message']['text'] == 'مدیریت لیست اعضا' && $is_admin[0]['status']) {
